d8c7474d2b849e6edf27955de94600b7718df9ce
Mazdak Farrokhzad <twingoow@gmail.com>
2015-08-29 00:34:07 +0200

    added JSDoc to all exports in utils.js

--- utils.js
+++ utils.js
@@ -25,22 +25,45 @@ const events		= require('sdk/dom/events'),
 	  {partial, curry, compose} = require('sdk/lang/functional'),
 	  {isNull, isUndefined, isFunction}	= require('sdk/lang/type');
 
+/**
+ * A function that takes nothing and does nothing.
+ */
 const noop = () => {};
 exports.noop = noop;
 
+/**
+ * Returns true if the given object is a Window.
+ *
+ * @param  {*}    window  The object to test.
+ * @return {bool}         True if the given object is a window.
+ */
 const isWindow = window => !isUndefined( window.window ) && !isUndefined( window.window.window );
+exports.isWindow = isWindow;
 
+/**
+ * Returns the window object from a given Element belonging to that window.
+ * It returns itself if the object passed was a Window.
+ *
+ * @param  {Element|Window}  element  The element to get window of, or a window.
+ * @return {Window}                   The window of the element or itself if a window was passed.
+ */
 const domWindow = element => isWindow( element ) ? element : element.ownerDocument.defaultView;
 exports.domWindow = domWindow;
 
 /**
  * Returns all the open normal (navigator:browser) windows (chrome mode).
  *
- * @return Array Array of all open windows.
+ * @return {Window[]}  Array of all open windows.
  */
 const getAllWindows = () => window_utils.windows( 'navigator:browser', {includePrivate: true} );
 exports.getAllWindows = getAllWindows;
 
+/**
+ * Returns true if the value v is either null or undefined.
+ *
+ * @param  {*}  v     The value.
+ * @return {bool}     True if either null or undefined.
+ */
 const nullOrUndefined = v => isNull( v ) || isUndefined( v );
 exports.nullOrUndefined = nullOrUndefined;
 
@@ -85,11 +108,38 @@ const unloader = (() => {
 		},
 	}
 })();
+
+/**
+ * Calls all unloaders and removes them after.
+ */
 exports.unload = unloader.runAll;
+
+/**
+ * Registers an unloader running callback and optionally a container that when unloaded will unregister the unloader.
+ *
+ * @param {function}        callback     The callback to run on unload.
+ * @param {Element|Window}  [container]  An optional container which when unloader will deregister the unloader.
+ */
 exports.unloader = unloader.register;
+
+/**
+ * Returns an unloader function where the second argument (component) is bound.
+ * In other words, partial application from the right in functional terms of the function unloader.
+ *
+ * @param  {component}  component  The component to partially apply from the right.
+ * @return {function}              The partially applied unloader function.
+ */
 exports.unloaderBind = component => callback => unloader.register( callback, component );
 
-// Replace a value with another value or a function of the original value
+/**
+ * Replace a value of a property with another value or a function of the original value.
+ * On unload, the change is reversed.
+ *
+ * @param  {Window} window A DOM Window.
+ * @param  {Object} obj    The object to change property on.
+ * @param  {string} prop   The property to change value of.
+ * @param  {*}      val    The value to set.
+ */
 const change = (window, obj, prop, val) => {
 	let orig = obj[prop];
 	obj[prop] = isFunction( val ) ? val( orig ) : val;
@@ -97,7 +147,7 @@ const change = (window, obj, prop, val) => {
 }
 exports.change = change;
 
-const undoListener = (element, type, listener, capture ) => {
+const undoListener = (element, type, listener, capture) => {
 	const undoListen = () => events.removeListener( element, type, listener, capture );
 	const undoUnload = partial( unloader.register, undoListen, domWindow( element ) );
 	return () => {
@@ -106,18 +156,50 @@ const undoListener = (element, type, listener, capture ) => {
 	}
 }
 
+/**
+ * Registers listener on element on type events.
+ * Automatically removed on unload.
+ *
+ * @param  {Element}  element  The DOM Element to listen for events on.
+ * @param  {string}   type     The type of event to listen on.
+ * @param  {function} listener The listener (callback) to register.
+ * @param  {Boolean}  capture  Bubbling related, see addEventListener. Default value: false.
+ * @return {function}          A function that undoes the registration.
+ */
 const on = (element, type, listener, capture = false) => {
 	events.on( element, type, listener, capture );
 	return undoListener( element, type, listener, capture );
 }
 exports.on = on;
 
+/**
+ * Registers listener on element on type events.
+ * This listener is only called once.
+ * Automatically removed on unload.
+ *
+ * @param  {Element}  element  The DOM Element to listen for events on.
+ * @param  {string}   type     The type of event to listen on.
+ * @param  {function} listener The listener (callback) to register.
+ * @param  {Boolean}  capture  Bubbling related, see addEventListener. Default value: false.
+ * @return {function}          A function that undoes the registration.
+ */
 const once = (element, type, listener, capture = false) => {
 	events.once( element, type, listener, capture );
 	return undoListener( element, type, listener, capture );
 }
 exports.once = once;
 
+/**
+ * Registers listener on element on type events.
+ * This registers the listener on multiple event types.
+ * Automatically removed on unload.
+ *
+ * @param  {Element}  element  The DOM Element to listen for events on.
+ * @param  {string[]} type     The type of events to listen on.
+ * @param  {function} listener The listener (callback) to register.
+ * @param  {Boolean}  capture  Bubbling related, see addEventListener. Default value: false.
+ * @return {function}          A function that undoes the registration.
+ */
 const onMulti = (element, types, listener, capture = false) => {
 	return types.map( type => on( element, type, listener ) );
 }
@@ -126,8 +208,8 @@ exports.onMulti = onMulti;
 /**
  * Apply a callback to each open and new browser windows.
  *
- * @usage watchWindows(callback): Apply a callback to each browser window.
- * @param [function] callback: 1-parameter function that gets a browser window.
+ * @usage watchWindows( callback ): Apply a callback to each browser window.
+ * @param {function} callback: 1-parameter function that gets a browser window.
  */
 const watchWindows = callback => {
 	// Change: Not running on load, no need.
@@ -141,14 +223,39 @@ const watchWindows = callback => {
 }
 exports.watchWindows = watchWindows;
 
+/**
+ * Computes the bounding width of an element.
+ *
+ * @param {Element} element The element.
+ * @return {string} The bounding width.
+ */
 const boundingWidth = element => element.getBoundingClientRect().width;
 exports.boundingWidth = boundingWidth;
 
-const px = element => element + 'px';
+/**
+ * Adds px unit to a value.
+ *
+ * @param  {string} v The value.
+ * @return {string} The value with px added to it.
+ */
+const px = v => v + 'px';
 exports.px = px;
 
+/**
+ * Computes the bounding width of an element and adds px unit to it.
+ *
+ * @param {Element}   element  The element.
+ * @return {string}            The bounding width + px unit.
+ */
 const boundingWidthPx = compose( px, boundingWidth );
 exports.boundingWidthPx = boundingWidthPx;
 
+/**
+ * Sets the width of an element.
+ * The function is curried by default.
+ *
+ * @param  {Element}   element  The element to set width of.
+ * @param  {string}    width    The width to set on element.
+ */
 const setWidth = curry( (element, width) => element.style.width = width );
 exports.setWidth = setWidth;
